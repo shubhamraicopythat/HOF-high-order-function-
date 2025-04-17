07cc310ea76e7dadd2d94681aa733987f8690e45
shubhamraicopythat <quicktalkme@gmail.com>
2025-04-17 23:50:24 +0530

    Hello Sir

--- learnjs.js
+++ learnjs.js
@@ -4,4 +4,14 @@ const powertwo = (n) => {
 function powercube(powertwo, n) {
   return powertwo(n) * n;
 }
-console.log(powercube(powertwo, 3));
+// console.log(powercube(powertwo, 3));
+
+function sayHello() {
+  return () => {
+    console.log("Hello Sir");
+  };
+}
+
+let guessvalue = sayHello();
+console.log(guessvalue);
+guessvalue();
